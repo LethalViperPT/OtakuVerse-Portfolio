67d13a25b6650371ea38e5295ff6dc2029dcb2b6
LethalViperPT <diana.santos.007.DS@gmail.com>
2026-07-01 17:28:58 +0100

    fix: ajustar uvicorn para server.py e desativar reload

--- backend/server.py
+++ backend/server.py
@@ -309,4 +309,4 @@ async def shutdown_db_client():
 if __name__ == '__main__':
     import uvicorn
     port = int(os.getenv("PORT", 8000))
-    uvicorn.run("server:app", host="0.0.0.0", port=port, reload=True)
+    uvicorn.run("server:app", host="0.0.0.0", port=port)
